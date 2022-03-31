8005f959a42f7bfeffc77f6b41de63bd9220d2e8
s190502 <jason.bowerman@utsouthwestern.edu>
2022-03-31 13:44:57 -0500

    got basic filetest project setup

--- DjangoPractice/urls.py
+++ DjangoPractice/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('filetest/', include('filetest.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
